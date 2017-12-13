237669a64eb86d5bf3fe0b221361b9dea4a638e4
dwainetrain <dwaine.best@gmail.com>
2017-12-13 14:58:28 -0800

    Adding Complete CH.02.00 Exercise, Incomplete CH.02.01

---
+++ Chapter 2 - Program Structure/00.triangle.js
@@ -0,0 +1,13 @@
+// Complete
+
+// Walk through
+// As you walk through pick up another '#'
+// Like burrs on your shoes while in the fields
+// At first it's just one
+// And then there's a whole gobb of them
+
+var burr = "";
+for (i = 0; i <= 7; i++ ) {
+  console.log(burr);
+  burr += "#";
+}
